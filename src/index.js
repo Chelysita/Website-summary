06013b3451364d642c19f7d9221b46bb44e2c8d3
Chelysita <ara.medinago@gmail.com>
2025-10-16 10:32:01 -0400

    Function to generate the summary added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-alert("Hello world");
+function generateSummary(event) {
+  event.preventDefault();
+
+  let summaryElement = document.querySelector("#summary");
+  summaryElement.innerHTML = "resumen";
+}
+
+let summaryGenerator = document.querySelector("#form-generator");
+summaryGenerator.addEventListener("submit", generateSummary);
